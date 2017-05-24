ca044457d36cc758611800f0759b8dc4b1c0b82f
mayuri <mayuripachpande@yahoo.in>
2017-05-25 02:02:48 +0530

    Add generator config that is used

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,10 @@ module TrekOManiacs
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.orm false
+      g.assets false
+      g.test_framework  false
+    end
   end
 end
