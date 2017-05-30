43ce87c2a1b20f4e6a0b5f47837e257f773b34ab
mayuri <mayuripachpande@yahoo.in>
2017-05-30 10:06:37 +0530

    Move error rendering code to method for easy calling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def render_errors_for(object)
+    render 'layouts/errors', locals: { object: object }
+  end
 end
