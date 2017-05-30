cb10da481652b02a346a61fda9cf7208357f85dc
mayuri <mayuripachpande@yahoo.in>
2017-05-30 10:16:33 +0530

    temp commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user! # skip high voltage pages
 
   def render_errors_for(object)
     render 'layouts/errors', locals: { object: object }
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
       get :my_account
       patch :update_username
       patch :update_password
+      patch :update_email
+      patch :update_contact
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
