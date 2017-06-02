5b54a966a9474907e001b019d5aaed1fc3061d48
mayuri <mayuripachpande@yahoo.in>
2017-06-02 07:50:09 +0530

    Changes to include username in registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def render_errors_for(object)
     render 'layouts/errors', locals: { object: object }
   end
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    end
 end
