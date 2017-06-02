45e37414469e7e67cc0f6f493a1e29a0eb2b67d0
Anand Bait <anand.bait@gmail.com>
2017-06-02 08:03:32 +0530

    Remove unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :venues
-  resources :routes
-  resources :profiles
+  resources :routes, except: [:new, :create]
+  resources :profiles, except: :new
   resources :events
-  resources :contacts
-  resources :addresses
+  resources :contacts, except: [:new, :create]
+  resources :addresses, except: [:new, :create]
   devise_for :users
   resources :users, except: [:new, :create] do
     collection do
