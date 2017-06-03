539208ccccbff9808d13cace3a09b699e86ab388
Anand Bait <anand.bait@gmail.com>
2017-06-03 07:38:10 +0530

    Venues Create, update and index

--- app/helpers/venues_helper.rb
+++ app/helpers/venues_helper.rb
@@ -1,2 +1,11 @@
 module VenuesHelper
+
+  def options_for_region
+    [
+      'Ahmednagar', 'Aurangabad', 'Beed', 'Belgaon', 'Belgaum', 'Dhule', 'Jalgaon', 'Kolhapur',
+      'Mumbai', 'Nashik', 'North Goa', 'Palghar', 'Pune', 'Raigad', 'Ratnagiri', 'Sangli', 'Satara',
+      'Sindhudurg', 'Sindhudurga', 'Solapur', 'Thane', 'Usmanabad'
+    ]
+  end
+
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -2,4 +2,9 @@ class Venue < ApplicationRecord
   has_many :routes
   has_many :contacts, as: :resource
   serialize :attractions
+
+  # setter
+  def attractions=(val)
+    super(val.split("\n").map(&:strip))
+  end
 end
