eb8e7b429f7dbd5cf972beda783ed211f95b9d99
Anand Bait <anand.bait@gmail.com>
2017-06-06 23:45:34 +0530

    Temp commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,30 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([[ name: 'Star Wars' ], [ name: 'Lord of the Rings' ]])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+{
+# Ahmednagar: [ 'Bahadurgad', 'Harishchandragad', 'Kalsubai', 'Pemgiri(Shahagad)', 'Sandan Valley - Karoli Ghat'],
+# Aurangabad: [ 'Bhangsigad(Bhangsi mata gad)', 'Devagiri (Daulatabad - Deogiri)', 'Janjala (Vaishagad)', 'Vetalwadi Gad'],
+# Beed: [ 'Dharmapuri'],
+# Belgaon: [ 'Vallabhgad'],
+# Belgaum: [ 'Belgaum Fort'],
+# Dhule: [ 'Laling', 'Raikot', 'Songir (Dhule)'],
+# Jalgaon: [ 'Amalner', 'Bahadarpur Fort', 'Parola'],
+# Kolhapur: [ 'Bhudargad', 'Gagangad', 'Gandharvgad', 'Kalanidhigad (Kalanandigad)', 'Mahipalgad', 'Mudagad', 'Panhalgad'],
+# Mumbai: [ 'Bandra Fort', 'Kala Killa', 'Madh Fort (Varsova Fort)', 'Mahim Fort', 'Riwa Fort', 'Sion Fort', 'Worli Fort'],
+# Nashik: [ 'Achala', 'Ahivant', 'Alang', 'Anjaneri', 'Aundha', 'Bhaskargad', 'Bitangad', 'Chaulher', 'Dehergad (Bhorgad)', 'Dermal', 'Dundha', 'Gadgada (Ghargad)', 'Galna', 'Hargad', 'Harihar', 'Hatgad', 'Indrai', 'Kanchan', 'Kanhergad', 'Kankrala', 'Katra', 'Kavnai', 'Kulang', 'Madangad ', 'Mangi-Tungi ', 'Markandeya', 'Moragad', 'Mordhan', 'Mulher', 'Nhavigad', 'Rajdher', 'Ramshej', 'Ranjangiri', 'Rawlya - Jawlya', 'Salher', 'Salota', 'Saptashrungi', 'Tringalwadi'],
+# 'North Goa': [ 'Terekhol Fort'],
+# Palghar: [ 'Ballalgad'],
+# Pune: [ 'Chakan Fort', 'Chavand ', 'Durg', 'Durg Dhakoba', 'Hadsar', 'Induri Fort (Gadhi)', 'Jivdhan', 'Korigad', 'Lohgad', 'Malhargad', 'Mohangad', 'Naneghat', 'Purandar', 'Raireshwar', 'Rajgad', 'Rohida', 'Shivneri', 'Sinhagad', 'Songir (Karjat)', 'Subhan Mangal', 'Tikona', 'Torna', 'Tung', 'Visapur'],
+# Raigad: [ 'Avchitgad', 'Bhimashankar', 'Bhivgad(Bhimgad)', 'Birwadi', 'Chambhargad', 'Colaba', 'Dhak-Bahiri', 'Ghangad', 'Ghosalgad', 'Janjira', 'Karnala', 'Khanderi', 'Khubladha Fort (Thal Fort)', 'Kokandiwa', 'Kondhavi', 'Korlai', 'Kurdugad', 'Lingana', 'Mangad', 'Mangalgad', 'Mirgad(Songir)', 'Mrugagad', 'Panhaledurg', 'Peb', 'Peth (Kothaligad)', 'Raigad', 'Ratnagad', 'Revdanda', 'Sagargad', 'Samrajgad', 'Sarasgad', 'Sarjekot (Alibaug)', 'ShivtharGhal', 'Sondai', 'Songiri (near Roha)', 'Sudhagad', 'Talgad', 'Underi'],
+# Ratnagiri: [ 'Ambolgad', 'Bankot', 'Barvai', 'Bhavanigad', 'Fattegad', 'Goa Fort', 'Gopalgad', 'Gowalkot', 'Kanakdurg', 'Mahipatgad', 'Mandangad', 'Manikdurg', 'Palgad', 'Rasalgad', 'Ratnadurg', 'Sumargad', 'Suvarnadurg'],
+# Sangli: [ 'Bhupalgad (Banurgad)', 'Dategad'],
+# Satara: [ 'Ajinkyatara', 'Bhairavgad (Satara)', 'Chandan-Vandan', 'Kalyangad(Nandgiri)', 'Kamalgad', 'Mahimangad', 'Pandavgad', 'Pratapgad', 'Sadashivgad', 'Sajjangad', 'Santoshgad', 'Vairatgad', 'Varugad', 'Vasantgad', 'Vasota'],
+# Sindhudurg: [ 'Bhagwantgad', 'Bharatgad', 'Hanumantgad', 'Kharepatan fort', 'Kotkamate', 'Mahadevgad', 'Narayangad(Amboli)', 'Ramgad', 'Shivgad', 'Siddhagad (Malvan)', 'Vetalgad', 'Yashawantgad (Redi Fort)'],
+# Sindhudurga: [ 'Nivati Fort', 'Padmgad (Malvan)', 'Rajkot', 'Sarjekot(Malvan)', 'Sindhudurg'],
+# Solapur: [ 'Akluj Fort', 'Dhotri Fort (Gadhi)', 'Machnur', 'Piliv Fort'],
+# Thane: [ 'Ajoba', 'Arnala', 'Asherigad', 'Balwantgad', 'Belapur Fort', 'Bhavangad', 'Bhupatgad', 'Chanderi', 'Danda Fort', 'Dharavi Fort', 'Durgadi Fort', 'Gambhirgad', 'Ghodbunder Fort', 'Gorakhgad', 'Irshalgad', 'Kaldurg', 'Kamandurg', 'Kelve Fort', 'Kelve Pankot', 'Kohoj', 'Mahim Fort ( Kelve - Mahim)', 'Mahuli', 'Malanggad', 'Nagla Bunder Fort', 'Shirgaon', 'Tahuli', 'Takmak', 'Tandulwadi', 'Vajragad (Vasai)', 'Vasai'],
+# Usmanabad: [ 'Naldurg', 'Paranda']
+}
