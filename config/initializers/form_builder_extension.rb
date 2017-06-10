6f59748aec05b793efeac683a285749608d26927
Anand Bait <anand.bait@gmail.com>
2017-06-10 21:10:45 +0530

    Refactoring and new helper for link_to_add_array_field

--- config/initializers/form_builder_extension.rb
+++ config/initializers/form_builder_extension.rb
@@ -1,9 +1,10 @@
 class ActionView::Helpers::FormBuilder
   include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::UrlHelper
   include ActionView::Context
 
   def amount_field(method, options={})
-    options[:class] = add_class(options)
+    options[:class] = add_class(options, 'input-group-field')
     content_tag :div, class: 'input-group' do
       content_tag(:span, options[:currency].html_safe || '$', class: 'input-group-label') +
       number_field(method, options)
@@ -26,19 +27,23 @@ class ActionView::Helpers::FormBuilder
     end.html_safe
   end
 
+  def link_to_add_array_field(text, options={})
+    link_to text, 'javascript:void(0)', class: add_class(options, 'button clone-array-field')
+  end
+
   private
 
-  def add_class(opts, class_name = 'input-group-field')
+  def add_class(opts, class_name)
     (classes = opts[:class] || '').concat(classes.is_a?(Array) ? [class_name] : " #{class_name}")
   end
 
   def build_array_field(method, options)
     options.merge!(multiple: true)
-    options[:class] = add_class(options)
+    options[:class] = add_class(options, 'input-group-field')
     content_tag :div, class: 'input-group' do
       text_field(method, options) +
       content_tag(:div, class: 'input-group-button') do
-        "<a href='javascript:void(0)' class='button alert delete-array-field'>#{'&times'.html_safe}</a>".html_safe
+        link_to '&times'.html_safe, 'javascript:void(0)', class: 'button alert delete-array-field'
       end
     end
   end
