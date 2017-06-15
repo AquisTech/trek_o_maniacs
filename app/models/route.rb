51eca371c1b9f1ddb1cae5e52c48a3240ed03415
Anand Bait <anand.bait@gmail.com>
2017-06-15 10:07:56 +0530

    Basic Validations for Route

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,18 @@
 class Route < ApplicationRecord
-  belongs_to :venue, inverse_of: :routes
+
+  # serialize attributes
   serialize :modes_of_transport
+  # validations
+  validates :name, presence: true
+  validates :start_point, presence: true
+  validates :via, presence: true
+  validates :end_point, presence: true
+  validates :description, presence: true
+  validates :distance, presence: true, numericality: { greater_than: 0, less_than: 5000 }
+  validates :modes_of_transport, presence: true
+
+  # Associations
+  # belongs_to
+  belongs_to :venue, inverse_of: :routes
+
 end
