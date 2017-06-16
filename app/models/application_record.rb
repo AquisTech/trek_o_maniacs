7c57694628ebfffa3ae77e504c1bcf1c97e201fb
Anand Bait <anand.bait@gmail.com>
2017-06-16 06:59:38 +0530

    Error displaying mechanism

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,18 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def all_error_messages
+    errors = {}
+    self.errors.messages.each do |attr, msg|
+      errors["#{self.class.name.downcase}[#{attr}]"] = msg.first
+    end
+    self.class.reflect_on_all_associations.each do |assoc|
+      send(assoc.name).each_with_index do |obj, i|
+        obj.errors.messages.each do |attr, msg|
+          errors["#{self.class.name.downcase}[#{assoc.name}_attributes][#{i}][#{attr}]"] = msg.first
+        end
+      end
+    end
+    errors
+  end
 end
